0e128b7400a7c52ddf42c8a9a90f58213738fbeb
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2021-08-15 17:26:49 +0200

    feat: add enqueue style and script functions

--- functions.php
+++ functions.php
@@ -1,6 +1,40 @@
 <?php
+
 /**
  * Theme functions
  * 
  * @package Naval Architecture
  */
+
+function naval_arch_enqueue_scripts()
+{
+  wp_register_style(
+    'main',
+    get_stylesheet_uri(),
+    [],
+    filemtime(get_stylesheet_directory() . '/style.css'),
+    'all'
+  );
+  wp_register_script(
+    'main',
+    get_template_directory_uri() . '/assets/main.js',
+    [],
+    filemtime(get_template_directory() . '/assets/main.js'),
+    true
+  );
+
+  // another way to immediately enqueue
+  // wp_enqueue_style('main', get_stylesheet_uri(), [], filemtime(get_stylesheet_directory() . '/style.css'), 'all');
+  // wp_enqueue_script(
+  //   'main',
+  //   get_template_directory_uri() . '/assets/main.js',
+  //   [],
+  //   filemtime(get_template_directory() . '/assets/main.js'),
+  //   true
+  // );
+
+  wp_enqueue_style('main');
+  wp_enqueue_script('main');
+}
+
+add_action('wp_enqueue_scripts', 'naval_arch_enqueue_scripts');
