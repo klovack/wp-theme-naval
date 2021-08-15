d9692599220b81a98b67b9e28294d0be59c91b60
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2021-08-15 19:17:45 +0200

    feat: add title to the page

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title('|',true,'right'); bloginfo('name'); ?></title>
   <?php
   wp_head();
   ?>
