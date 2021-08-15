12c50d2984df22dc2cb20245116a119d2542f314
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2021-08-15 18:29:34 +0200

    feat: integrate bootstrap into the theme

--- functions.php
+++ functions.php
@@ -6,8 +6,40 @@
  * @package Naval Architecture
  */
 
+
+/**
+ * @return string handle name for the bootstrap css and js that you can register
+ */
+function naval_register_bootstrap() {
+  $handle_name = 'bootstrap';
+  $css_path = '/css/bootstrap.min.css';
+  $js_path = '/js/bootstrap.bundle.min.js';
+  $bootstrap_path_uri = get_template_directory_uri() . '/assets/src/lib/bootstrap';
+
+  // Register Styles
+  wp_register_style(
+    $handle_name,
+    $bootstrap_path_uri . $css_path,
+    [],
+    false
+  );
+
+
+  // Register Scripts
+  wp_register_script(
+    $handle_name,
+    $bootstrap_path_uri . $js_path,
+    [],
+    false,
+    true
+  );
+
+  return $handle_name;
+}
+
 function naval_arch_enqueue_scripts()
 {
+  // Register Styles
   wp_register_style(
     'main',
     get_stylesheet_uri(),
@@ -15,6 +47,8 @@ function naval_arch_enqueue_scripts()
     filemtime(get_stylesheet_directory() . '/style.css'),
     'all'
   );
+
+  // Register Scripts
   wp_register_script(
     'main',
     get_template_directory_uri() . '/assets/main.js',
@@ -33,7 +67,12 @@ function naval_arch_enqueue_scripts()
   //   true
   // );
 
+  // Register bootstrap
+  $bootstrap_handle_name = naval_register_bootstrap();
+
+  wp_enqueue_style($bootstrap_handle_name);
   wp_enqueue_style('main');
+  wp_enqueue_script($bootstrap_handle_name);
   wp_enqueue_script('main');
 }
 
