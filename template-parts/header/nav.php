0d95cf512ceb2e12855664dc78b3a4f4fee6b845
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2021-08-15 19:06:36 +0200

    feat: add navigation and refactor header into template-parts

---
+++ template-parts/header/nav.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Navigation template
+ * 
+ * @package Naval Architecture
+ */
+?>
+
+<header id="masthead" class="site-header d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+  <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+      <use xlink:href="#bootstrap"></use>
+    </svg>
+  </a>
+
+  <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 row g-2">
+    <div class="col-auto">
+      <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+    </div>
+
+    <div class="col-auto">
+      <button type="submit" class="btn btn-primary mb-3">
+        <i class="bi bi-search"></i>
+      </button>
+    </div>
+  </form>
+
+  <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 text-end">
+    <li><a href="#" class="nav-link px-2 link-secondary">Categories</a></li>
+    <li><a href="#" class="nav-link px-2 link-dark">Blog</a></li>
+  </ul>
+</header>
